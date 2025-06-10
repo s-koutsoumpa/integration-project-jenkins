5d5086f459889fc471ecb7f0163c997413fdcb9e
s-koutsoumpa <stella.koutsoumpa12@gmail.com>
2025-06-10 20:40:47 +0300

    Add the flask app

---
+++ app/flask-app.py
@@ -0,0 +1,16 @@
+from flask import Flask, jsonify
+from pymongo import MongoClient
+
+app = Flask(__name__)
+client = MongoClient("mongodb://mongodb:27017/")
+db = client["testdb"]
+collection = db["items"]
+
+@app.route("/")
+def index():
+    collection.insert_one({"msg": "Hello from MongoDB"})
+    doc = collection.find_one(sort=[('_id', -1)])
+    return jsonify({"message": doc["msg"]})
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
