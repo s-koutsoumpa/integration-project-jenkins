ac01257a43e45db22f8d25b1d8bcf7ac7878578b
s-koutsoumpa <stella.koutsoumpa12@gmail.com>
2025-06-11 18:46:34 +0300

    Add test_integration.sh file

---
+++ test_integration.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo "Testing started..."
+sleep 5
+response=$(curl -s http://localhost:5000)
+
+if echo "$response" | grep -q "Hello from MongoDB"; then
+  echo "Successful Test"
+  exit 0
+else
+  echo "Test failed"
+  exit 1
+fi
