c8363cf702cab8b9d9743943388467bb586dfae2
s-koutsoumpa <stella.koutsoumpa12@gmail.com>
2025-06-12 19:11:04 +0300

    Updated flask app

--- app/flask-app.py
+++ app/flask-app.py
@@ -6,8 +6,8 @@ client = MongoClient("mongodb://mongodb:27017/")
 db = client["testdb"]
 collection = db["items"]
 
-@app.route("/")
-def index():
+@app.route("/", methods=["GET"])
+def start_page():
     collection.insert_one({"msg": "Hello from MongoDB"})
     doc = collection.find_one(sort=[('_id', -1)])
     return jsonify({"message": doc["msg"]})
